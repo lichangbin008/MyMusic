f6a247408fd9ac62541344f88acd005cef81a978
lichangbin008 <lichangbin008@163.com>
2019-11-26 16:03:19 +0800

    提交初始工程

---
+++ app/src/main/java/com/ixuea/courses/mymusic/util/Consts.java
@@ -0,0 +1,12 @@
+package com.ixuea.courses.mymusic.util;
+
+import com.ixuea.courses.mymusic.BuildConfig;
+
+/**
+ * Created by ${lichangbin} on 2019/11/24.
+ */
+
+public class Consts {
+    public static final String RESOURCE_PREFIX = BuildConfig.RESOURCE_PREFIX;
+    public static final String ENDPOINT = BuildConfig.ENDPOINT;
+}
